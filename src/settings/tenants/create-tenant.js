6b26e9402ac2110410d3dc61b3c56cbea8c799ee
Ferley Leon <ferlellws@gmail.com>
2022-09-05 12:11:28 -0500

    Se hacen ajustes al tenant

--- src/settings/tenants/create-tenant.js
+++ src/settings/tenants/create-tenant.js
@@ -3,7 +3,9 @@
 const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
 const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-
+const cognito = new AWS.CognitoIdentityServiceProvider({
+    apiVersion: "2016-04-18"
+});
 
 const TABLE_NAME = process.env.TENANTS_TABLE
 
@@ -11,26 +13,78 @@ AWS.config.update({
     region: "us-east-1",
 });
 
+
 exports.lambdaHandler = async (event) => {
-    const tenant = event.queryStringParameters.name;
+    const tenant = event.queryStringParameters.tenant;
+    const environment = event.queryStringParameters.environment;
 
     const date = new Date();
 
     const item = {
-        id: AWS.util.uuid.v4(),
         tenant: tenant,
+        environment: environment,
         date: date.toLocaleDateString()
     }
 
     const existItem = await existsItem(tenant);
 
-    // const savedItem = await saveItem(item);
+    console.log("USER_POOL_ID: " + process.env.USER_POOL_ID);
+    console.log("TENANTS_TABLE: " + process.env.TENANTS_TABLE);
+    let body = '';
+    if (existItem) {
+        body = {
+            msg: "El tenant: " + tenant + " ya existe"
+        }
+    } else {
+        const savedItem = await saveItem(item);
+
+        // const userSuperAdmin = {
+        //     UserPoolId: process.env.USER_POOL_ID,
+        //     Username: 'superadmin',
+        //     UserAttributes: [
+        //         {
+        //             Name: 'email',
+        //             Value: 'ferley.leon@tecno.co'
+        //         },
+        //         {
+        //             Name: 'custom:tenant',
+        //             Value: tenant
+        //         },
+        //         {
+        //             Name: 'custom:environment',
+        //             Value: environment
+        //         }
+        //     ]
+        // }
+        // await saveUserCognito(userSuperAdmin);
+
+        const userAdmin = {
+            UserPoolId: process.env.USER_POOL_ID,
+            Username: 'admin',
+            UserAttributes: [
+                {
+                    Name: 'email',
+                    Value: 'ferlellws@gmail.com'
+                },
+                {
+                    Name: 'custom:tenant',
+                    Value: tenant
+                },
+                {
+                    Name: 'custom:environment',
+                    Value: environment
+                }
+            ]
+        }
+        await saveUserCognito(userAdmin);
+        body = {...savedItem, ...{msg: "El tenant fue creado exitosamente"}};
+    }
 
     return {
         statusCode: 200,
-        // body: JSON.stringify(savedItem),
-        body: JSON.stringify(existItem),
-      }
+        body: JSON.stringify(body),
+    }
+
 }
 
 async function existsItem(tenant) {
@@ -41,8 +95,10 @@ async function existsItem(tenant) {
         }
 	};
 
-    return ddb.getItem(params).promise().then(() => {
-        return !data.Item;
+    return ddb.getItem(params).promise().then((data) => {
+        return !!(data.Item);
+    }, (err) => {
+        console.log("====================");
     });
 }
 
@@ -54,10 +110,6 @@ async function saveItem(item) {
 
     console.log(params)
     
-    // return dynamo.put(params).promise().then(() => {
-    //     return item;
-    // });
-
     return dynamo.put(params).promise().then(() => {
         return item;
     });
@@ -65,3 +117,8 @@ async function saveItem(item) {
 
     // TODO: Crear en people dev-transport-[tenant]
 };
+
+async function saveUserCognito(user) {
+    const result = await cognito.adminCreateUser(user).promise();
+    return result;
+}
